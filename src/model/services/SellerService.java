e33aea852a41747b3d94994a6a7f77fbd16c0131
Nelio Alves <acenelio@gmail.com>
2019-07-29 23:25:21 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,29 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+
+	private SellerDao dao = DaoFactory.createSellerDao();
+	
+	public List<Seller> findAll() {
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Seller obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Seller obj) {
+		dao.deleteById(obj.getId());
+	}
+}
